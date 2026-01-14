573e7c35c554f4e3fef9b817d73a4fb4209806bf
webst149 <nathaniel.webster@mail.utoronto.ca>
2026-01-14 01:36:44 -0500

    mostly complete

--- src/Description.jsx
+++ src/Description.jsx
@@ -5,7 +5,7 @@ const Description = ({ description, ref }) => {
     return (
         <div className="descript-section">
             <img src="src\profile-picture.jpg" alt="Picture of myself" className="personal-picture"/>
-            <div className="descript-container" ref={ref}>
+            <div className="descript-container scroll-target" ref={ref}>
                 <p className="text-2xl">{description}</p>
             </div>
         </div>
