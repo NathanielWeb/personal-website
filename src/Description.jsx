d4a451b90c2d2efd26cdbcc13a395093905645a7
webst149 <nathaniel.webster@mail.utoronto.ca>
2026-01-14 22:57:17 -0500

    fixing picture glitch

--- src/Description.jsx
+++ src/Description.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
+import profilePic from './assets/profile-picture.jpg'
 import './App.css'
 
 const Description = ({ description, ref }) => {
     return (
         <div className="descript-section">
-            <img src="src\profile-picture.jpg" alt="Picture of myself" className="personal-picture"/>
+            <img src={profilePic} alt="Picture of myself" className="personal-picture"/>
             <div className="descript-container scroll-target" ref={ref}>
                 <p className="text-2xl">{description}</p>
             </div>
